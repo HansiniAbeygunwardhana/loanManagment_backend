b8d7a1156e7ccc73c7046c8bcf662d72689d88a0
HansiniAbyegunawardhana <hchathupomoda@gmail.com>
2023-05-16 10:35:14 +0530

    first commit

--- loanManagment/settings.py
+++ loanManagment/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'loanManagment',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,17 @@ WSGI_APPLICATION = 'loanManagment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'project_db',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'root',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
