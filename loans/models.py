625a9b2e37fc2d1d5ea42ac46f31bab8218997d5
HansiniAbyegunawardhana <hchathupomoda@gmail.com>
2023-06-23 23:49:04 +0530

    adding loan api

--- loans/models.py
+++ loans/models.py
@@ -6,22 +6,19 @@ from django.conf import settings
 class Loan(models.Model):
     
     branches = (
-        ('polonnaruwa' , 'Polonnaruwa'),
-        ('diyasenpura' , 'Diyasenpura'),
-        ('sewanapitiya' , 'Sewanapitiya'),
-        ('dehiaththakandiya' , 'Dehiaththakandiya'),
-        ('mahiyanaganaya' , 'Mahiyanaganaya'),
+        ('Polonnaruwa' , 'Polonnaruwa'),
+        ('Diyasenpura' , 'Diyasenpura'),
+        ('Sewanapitiya' , 'Sewanapitiya'),
+        ('Dehiaththakandiya' , 'Dehiaththakandiya'),
+        ('Mahiyanaganaya' , 'Mahiyanaganaya'),
         )
     
-    loan_id = models.IntegerField(primary_key=True)
-    loan_name = models.CharField(max_length=100)
-    branch_location = models.CharField(max_length=20, choices=branches)
-    loan_amount = models.FloatField()
+    loan_id = models.AutoField(primary_key=True)
     username = models.ForeignKey(User, on_delete=models.SET_NULL , null=True)
+    loaned_date = models.DateField( default='2020-01-01' , null=False)
+    branch_location = models.CharField(max_length=20, choices=branches)
+    loaned_amount = models.FloatField(null=False)
     bike_number = models.CharField(max_length=12)
     first_guarantor = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='first_guarantor', null=True)
     second_guarantor = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='second_guarantor', null=True)
     
-
-    def __str__(self):
-        return self.loan_name
--- loans/serializers.py
+++ loans/serializers.py
@@ -1,7 +1,22 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import  serializers	
 from .models import Loan
+from users.models import User
 
-class loanSerializer(ModelSerializer):
+class loanSerializer(serializers.ModelSerializer):
+    
+    username = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+    first_guarantor = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+    second_guarantor = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+        
     class Meta:
         model = Loan
         fields = '__all__'
