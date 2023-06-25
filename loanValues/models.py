21ed310ae3c12b469bad32647ed390fa5b0ce7ef
HansiniAbyegunawardhana <hchathupomoda@gmail.com>
2023-06-25 23:09:43 +0530

    added loanvaluve logic

--- loanValues/models.py
+++ loanValues/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from loans.models import Loan
+import numpy as np
+from datetime import timedelta
 
 # Create your models here.
 
@@ -12,5 +14,29 @@ class loanValue (models.Model):
     balance = models.FloatField( null=False )
     
     
+    
+    ## Daily interest rate = 0.00115068
     def __str__(self):
-        return str(self.loan_number)
+        return str(self.balance) 
+    
+    def save(self, *args, **kwargs):
+        
+        interestrate = 42.0
+        interestperday = interestrate /365/100
+        
+        
+        last_record = loanValue.objects.filter(loan_number=self.loan_number).order_by('-id').first()
+        if last_record:
+            daysfrompayment = (self.payment_date - last_record.payment_date).days
+            self.interest = np.round(last_record.balance * interestperday * daysfrompayment , 2)
+            self.principle = np.round(self.payment_amout - self.interest , 2)
+            self.balance = last_record.balance - self.principle
+        else:
+            daysfrompayment = (self.payment_date - self.loan_number.loaned_date).days
+            self.interest = np.round(self.loan_number.loaned_amount * interestperday * daysfrompayment , 2)
+            self.principle = np.round(self.payment_amout - self.interest , 2)
+            self.balance = self.loan_number.loaned_amount - self.principle
+         
+        super(loanValue, self).save(*args, **kwargs)
+        
+        #TODO: maintaince balance with different loan accounts
